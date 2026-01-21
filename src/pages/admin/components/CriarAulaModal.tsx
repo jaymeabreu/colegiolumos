b71491643a4c8fda1458c25baed9e324edacfb07
jaymeabreu8 <jaymeabreu8@gmail.com>
2026-01-21 12:01:43 -0300

    Update CriarAulaModal.tsx

--- src/pages/admin/components/CriarAulaModal.tsx
+++ src/pages/admin/components/CriarAulaModal.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Plus } from 'lucide-react';
+import { Plus } from 'lucide-react'; 
 import { Button } from '../../../components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '../../../components/ui/dialog';
 import { Input } from '../../../components/ui/input';
