5315dc1e84b6760a071d01b469123dedd500d125
jaymeabreu8 <jaymeabreu8@gmail.com>
2026-02-07 08:40:22 -0300

    Update RendimentoTab.tsx

--- src/pages/diario/components/RendimentoTab.tsx
+++ src/pages/diario/components/RendimentoTab.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../..
 import { Button } from '../../../components/ui/button';
 import { Input } from '../../../components/ui/input';
 import { Label } from '../../../components/ui/label';
-import { Textarea } from '../../../components/ui/textarea';
+import { Textarea } from '../../../components/ui/textarea'; 
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '../../../components/ui/dialog';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../../../components/ui/select';
 import { Badge } from '../../../components/ui/badge';
