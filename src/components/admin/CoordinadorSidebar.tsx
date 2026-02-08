59501e7cfe909b2eef704d40c8a1bb07f809cc65
jaymeabreu8 <jaymeabreu8@gmail.com>
2026-02-08 14:53:57 -0300

    Update CoordinadorSidebar.tsx

--- src/components/admin/CoordinadorSidebar.tsx
+++ src/components/admin/CoordinadorSidebar.tsx
@@ -161,18 +161,14 @@ export function CoordinadorSidebar({ onTabChange }: CoordinadorSidebarProps) {
       )}
 
       {/* SIDEBAR */}
-     <div 
+      <div 
         className={`
-         <div 
-  className={`
-    fixed left-0 top-0 h-[100dvh] w-full min-[881px]:w-64 bg-white dark:bg-gray-900 
-    border-r border-gray-200 dark:border-gray-800 flex flex-col overflow-hidden
-    transition-transform duration-300 ease-in-out z-50
-    max-[880px]:-translate-x-full
-    ${mobileMenuOpen ? 'max-[880px]:translate-x-0' : ''}
-  `}
->
-        style={{ zIndex: mobileMenuOpen ? 50 : 10 }}
+          fixed left-0 top-0 h-[100dvh] w-full min-[881px]:w-64 bg-white dark:bg-gray-900 
+          border-r border-gray-200 dark:border-gray-800 flex flex-col overflow-hidden
+          transition-transform duration-300 ease-in-out z-50
+          max-[880px]:-translate-x-full
+          ${mobileMenuOpen ? 'max-[880px]:translate-x-0' : ''}
+        `}
       >
         {/* HEADER - LOGO E NOME DA ESCOLA */}
         <div className="p-6 border-b border-gray-200 dark:border-gray-800 flex-shrink-0">
